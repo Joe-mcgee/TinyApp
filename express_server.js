5b97dc6980c7ab3a0ef31b4532fec35ff145710b
Taylor <thulsmans.2994@me.com>
2018-01-23 21:34:39 +0000

    random number and form

--- express_server.js
+++ express_server.js
@@ -3,6 +3,9 @@ var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -12,9 +15,15 @@ app.get("/", (req, res) => {
   res.end("Hello!");
 });
 
-/*app.get("/urls.json", (req, res) => {
+app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-});*/
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+  console.log('get?')
+});
+
 
 app.get("/hello", (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
@@ -30,6 +39,23 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+function generateRandomString() {
+  // https://stackoverflow.com/questions/10726909/random-alpha-numeric-string-in-javascript
+  return Math.random().toString(36).slice(7)
+}
+
+
