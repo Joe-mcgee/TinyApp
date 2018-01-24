97aca086b53f86f0ed8bdc27b56f0b30b13848c0
Taylor <thulsmans.2994@me.com>
2018-01-24 01:34:12 +0000

    regex for potential missing http://

--- express_server.js
+++ express_server.js
@@ -1,15 +1,20 @@
 var express = require("express");
 var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+var PORT = process.env.PORT || 8080;
 app.set("view engine", "ejs");
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  // https://stackoverflow.com/questions/10726909/random-alpha-numeric-string-in-javascript
+  return Math.random().toString(36).slice(7);
+}
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -41,26 +46,26 @@ app.get("/urls/:id", (req, res) => {
 
 
 
-app.post("/urls", (req, res) => {  // debug statement to see POST parameters;
-  let random = generateRandomString()
-  urlDatabase[random] = req.body['longURL'];
-  res.redirect(`urls/${random}`)
-  });
+app.post("/urls", (req, res) => {
+  let random = generateRandomString();
+  let input = req.body['longURL'];
+  let httpRegex = RegExp('^http://');
+  if (httpRegex.test(input)) {
+    urlDatabase[random] = req.body['longURL'];
+  } else {
+    urlDatabase[random] = 'http://' + input;
+  }
+  res.redirect(`urls/${random}`);
+});
 
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL];
-  console.log(longURL)
   res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
-  console.log( `Example app listening on port ${PORT}!`);
+  console.log(`Example app listening on port ${PORT}!`);
 });
 
-
-function generateRandomString() {
-  // https://stackoverflow.com/questions/10726909/random-alpha-numeric-string-in-javascript
-  return Math.random().toString(36).slice(7)
-}
 
 
